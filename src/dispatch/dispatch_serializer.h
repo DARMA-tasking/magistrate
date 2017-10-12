441da57287868fb3098ff7db2ec086267819b922
Jonathan Lifflander <jliffla@sandia.gov>
2017-10-11 18:51:35 -0700

    serdes: fix non-detector variant

--- src/dispatch/dispatch_serializer.h
+++ src/dispatch/dispatch_serializer.h
@@ -12,15 +12,27 @@
 
 namespace serdes {
 
+// Implement a lightweight simple detection mechanism (when full detector is not
+// present) to detect type of serialize() method: intrusive/non-intrusive
+// variants
+#if !HAS_DETECTION_COMPONENT
 template <typename SerializerT, typename T>
-struct SerializerDispatch {
-  template <typename U>
-  using isByteCopyType =
-  typename std::enable_if<std::is_arithmetic<U>::value, T>::type;
+struct hasSerialize {
+  template <
+    typename C,
+    typename = decltype(std::declval<C>().serialize(std::declval<SerializerT&>()))
+  >
+  static std::true_type test(int);
 
-  template <typename U>
-  using isNotByteCopyType =
-  typename std::enable_if<not std::is_arithmetic<U>::value, T>::type;
+  template <typename C>
+  static std::false_type test(...);
+
+  static constexpr bool value = decltype(test<T>(0))::value;
+};
+#endif
+
+template <typename SerializerT, typename T>
+struct SerializerDispatchNonByte {
 
   template <typename U = T>
   void operator()(SerializerT& s, T* val, SizeType num) {
@@ -32,52 +44,65 @@ struct SerializerDispatch {
   #if HAS_DETECTION_COMPONENT
     template <typename U>
     using hasInSerialize =
-      typename std::enable_if<SerializableTraits<U>::has_serialize_instrusive, T>::type;
+    typename std::enable_if<SerializableTraits<U>::has_serialize_instrusive, T>::type;
+
     template <typename U>
     using hasNoninSerialize =
-      typename std::enable_if<SerializableTraits<U>::has_serialize_noninstrusive, T>::type;
-
-    template <typename U = T>
-    void apply(
-      SerializerT& s, T* val, SizeType num, hasInSerialize<U>* x = nullptr
-    ) {
-      debug_serdes("SerializerDispatch: intrusive serialize: val=%p\n", &val);
-      for (SizeType i = 0; i < num; i++) {
-        val[i].template serialize<SerializerT>(s);
-      }
-    }
-
-    template <typename U = T>
-    void apply(
-      SerializerT& s, T* val, SizeType num, hasNoninSerialize<U>* x = nullptr
-    ) {
-      debug_serdes("SerializerDispatch: non-intrusive serialize: val=%p\n", &val);
-      for (SizeType i = 0; i < num; i++) {
-        serialize(s, val[i]);
-      }
-    }
-
+    typename std::enable_if<SerializableTraits<U>::has_serialize_noninstrusive, T>::type;
   #else
     template <typename U>
-    using isSerializableType = isNotByteCopyType<U>;
+    using hasInSerialize =
+    typename std::enable_if<hasSerialize<SerializerT, U>::value, T>::type;
 
-    template <typename U = T>
-    void apply(
-      SerializerT& s, T* val, SizeType num, isSerializableType<U>* x = nullptr
-    ) {
-      debug_serdes("SerializerDispatch: serialize: val=%p\n", &val);
-      for (SizeType i = 0; i < num; i++) {
-        val[i].serialize(s);
-      }
-    }
+    template <typename U>
+    using hasNoninSerialize =
+    typename std::enable_if<!hasSerialize<SerializerT, U>::value, T>::type;
   #endif
 
   template <typename U = T>
   void apply(
+    SerializerT& s, T* val, SizeType num, hasInSerialize<U>* x = nullptr
+  ) {
+    debug_serdes("SerializerDispatch: intrusive serialize: val=%p\n", &val);
+    for (SizeType i = 0; i < num; i++) {
+      val[i].template serialize<SerializerT>(s);
+    }
+  }
+
+  template <typename U = T>
+  void apply(
+    SerializerT& s, T* val, SizeType num, hasNoninSerialize<U>* x = nullptr
+  ) {
+    debug_serdes("SerializerDispatch: non-intrusive serialize: val=%p\n", &val);
+    for (SizeType i = 0; i < num; i++) {
+      serialize(s, val[i]);
+    }
+  }
+};
+
+template <typename SerializerT, typename T>
+struct SerializerDispatch {
+  template <typename U>
+  using isByteCopyType =
+  typename std::enable_if<std::is_arithmetic<U>::value, T>::type;
+
+  template <typename U>
+  using isNotByteCopyType =
+  typename std::enable_if<not std::is_arithmetic<U>::value, T>::type;
+
+  template <typename U = T>
+  void operator()(
     SerializerT& s, T* val, SizeType num, isByteCopyType<U>* x = nullptr
   ) {
-    debug_serdes("SerializerDispatch: contiguousTyped: val=%p\n", &val);
     SerializerT::contiguousTyped(s, val, num);
+  }
+
+  template <typename U = T>
+  void operator()(
+    SerializerT& s, T* val, SizeType num, isNotByteCopyType<U>* x = nullptr
+  ) {
+    SerializerDispatchNonByte<SerializerT, T> dispatch;
+    dispatch(s, val, num);
   }
 
 };
