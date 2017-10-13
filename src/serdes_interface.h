f00a7bfad71d1a1a9ed29e8fdd77c2384e8452ab
Jonathan Lifflander <jliffla@sandia.gov>
2017-10-12 23:15:41 -0700

    serdes: update serialization interface, matches vt

--- src/serdes_interface.h
+++ src/serdes_interface.h
@@ -2,13 +2,25 @@
 #if !defined INCLUDED_SERDES_INTERFACE
 #define INCLUDED_SERDES_INTERFACE
 
+#include <cstdlib>
 #include <functional>
+#include <memory>
 
-namespace serialize { namespace interface {
+namespace serialization { namespace interface {
 
 using SizeType = size_t;
 using SerialByteType = char;
+
 using BufferCallbackType = std::function<SerialByteType*(SizeType size)>;
+
+struct SerializedInfo {
+  virtual SizeType getSize() const = 0;
+  virtual SerialByteType* getBuffer() const = 0;
+  virtual ~SerializedInfo() { }
+};
+
+using SerializedInfoPtrType = std::unique_ptr<SerializedInfo>;
+using SerializedReturnType = SerializedInfoPtrType;
 
 template <typename T>
 SerializedReturnType serialize(T& target, BufferCallbackType fn = nullptr);
@@ -16,6 +28,6 @@ SerializedReturnType serialize(T& target, BufferCallbackType fn = nullptr);
 template <typename T>
 T* deserialize(SerialByteType* buf, SizeType size, T* user_buf = nullptr);
 
-}} /* end namespace serialize::interface */
+}} /* end namespace serialization::interface */
 
 #endif /*INCLUDED_SERDES_INTERFACE*/
