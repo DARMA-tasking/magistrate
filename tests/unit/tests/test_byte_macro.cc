4bf642647ef974c03b773e26ff7432595a4604da
Jonathan Lifflander <jliffla@sandia.gov>
2017-10-13 23:44:41 -0700

    serdes: add unit test for byte serialize macro

---
+++ tests/unit/tests/test_byte_macro.cc
@@ -0,0 +1,55 @@
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "test_harness.h"
+
+#include "serdes_headers.h"
+#include "serialization_library_headers.h"
+
+#include <vector>
+#include <cstdio>
+
+namespace userTest {
+
+static constexpr int const my_val_init = 29;
+
+struct UserObject1 {
+  int my_val;
+
+  void init() {
+    my_val = my_val_init;
+  }
+
+  void check() {
+    EXPECT_EQ(my_val, my_val_init);
+  }
+};
+
+} // end namespace userTest
+
+SERDES_BYTES(::userTest::UserObject1);
+
+namespace serdes { namespace tests { namespace unit {
+
+struct TestByteMacro : TestHarness { };
+
+TEST_F(TestByteMacro, test_bytecopy_trait) {
+  using namespace ::serialization::interface;
+
+  using TestType = ::userTest::UserObject1;
+  TestType t;
+  t.init();
+  t.check();
+
+  auto ret = serialization::interface::serialize<TestType>(t);
+
+  auto tptr = serialization::interface::deserialize<TestType>(
+    ret->getBuffer(), ret->getSize()
+  );
+  auto& t_final = *tptr;
+
+  t_final.check();
+}
+
+}}} // end namespace serdes::tests::unit
