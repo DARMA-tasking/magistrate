d08572ce93cb18c783f43b3e3c17144f0487e5e3
Jonathan Lifflander <jliffla@sandia.gov>
2017-10-13 17:58:20 -0700

    serdes: add new trait for checking byte copyability

---
+++ tests/unit/tests/test_bytecopy_trait.cc
@@ -0,0 +1,45 @@
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "test_harness.h"
+
+#include "serdes_headers.h"
+#include "serialization_library_headers.h"
+
+#include <vector>
+#include <cstdio>
+
+#define TEST_BYTE_DEBUG_PRINT 1
+
+namespace serdes { namespace tests { namespace unit {
+
+struct TestByteCopyTrait : TestHarness { };
+
+struct ByteCopyStruct {
+  using isByteCopyable = std::true_type;
+
+  int x = 10, y = 20;
+};
+
+TEST_F(TestByteCopyTrait, test_bytecopy_trait) {
+  using namespace ::serialization::interface;
+
+  using TestType = ByteCopyStruct;
+  TestType t{100,200};
+
+  auto ret = serialization::interface::serialize<TestType>(t);
+
+  #if TEST_BYTE_DEBUG_PRINT
+    printf("buffer=%p, size=%ld\n", ret->getBuffer(), ret->getSize());
+  #endif
+
+  auto tptr = serialization::interface::deserialize<TestType>(
+    ret->getBuffer(), ret->getSize()
+  );
+  auto& dest = *tptr;
+
+  printf("ByteCopyStruct {%d,%d}\n", dest.x, dest.y);
+}
+
+}}} // end namespace serdes::tests::unit
