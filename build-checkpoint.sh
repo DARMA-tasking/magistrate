cae10db3b9cddee5d18e0eff3839cda50536f099
Jonathan Lifflander <jliffla@sandia.gov>
2018-06-12 12:16:42 -0700

    add more compiler options

--- build-checkpoint.sh
+++ build-checkpoint.sh
@@ -31,14 +31,8 @@ fi
 compiler_c=clang-mp-3.9
 compiler_cxx=clang++-mp-3.9
 
-if test $# -gt 4
-then
-    if test $5 = "gnu"
-    then
-        compiler_c=gcc
-        compiler_cxx=g++
-    fi
-fi
+if test $# -gt 4; then; compiler_c=$5; fi
+if test $# -gt 5; then; compiler_cxx=$6; fi
 
 cmake ../checkpoint                                                    \
       -DCMAKE_INSTALL_PREFIX=../checkpoint-install                     \
@@ -48,5 +42,5 @@ cmake ../checkpoint                                                    \
       -DCHECKPOINT_BUILD_TESTS:bool=ON                                 \
       -DCHECKPOINT_BUILD_EXAMPLES:bool=ON                              \
       -DGTEST_DIR=${gtest_directory}                                   \
-      -DCMAKE_BUILD_TYPE=debug                                         \
+      -DCMAKE_BUILD_TYPE=${build_mode}                                 \
       -DCMAKE_EXPORT_COMPILE_COMMANDS=true
