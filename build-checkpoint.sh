cc5190c2444bf410c6e7e04ac27a68fc27373cda
Jonathan Lifflander <jliffla@sandia.gov>
2018-11-01 15:48:47 -0700

    #3: build: vt-auto-build add kokkos argument

--- build-checkpoint.sh
+++ build-checkpoint.sh
@@ -35,6 +35,14 @@ if test $# -gt 4; then build_tests=$5; fi
 if test $# -gt 5; then compiler_c=$6; fi
 if test $# -gt 6; then compiler_cxx=$7; fi
 
+if test $# -gt 7
+then
+    kokkos_path_str="-Dkokkos_DIR=$8"
+else
+    kokkos_path_str=
+fi
+
+
 cmake ../checkpoint                                                    \
       -DCMAKE_INSTALL_PREFIX=../checkpoint-install                     \
       -DCMAKE_CXX_COMPILER=${compiler_cxx}                             \
@@ -44,7 +52,5 @@ cmake ../checkpoint                                                    \
       -DCHECKPOINT_BUILD_EXAMPLES:bool=${build_tests}                  \
       -Dgtest_DIR=${gtest_directory}                                   \
       -DCMAKE_BUILD_TYPE=${build_mode}                                 \
+      ${kokkos_path_str}                                               \
       -DCMAKE_EXPORT_COMPILE_COMMANDS=true
-
-
-#      -Dkokkos_DIR=/Users/jliffla/codes/kokkos-install-cmake/lib/      \
