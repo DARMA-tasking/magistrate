a791f76bf4a3e84c207f953f7dd3efcbf0e48022
Jonathan Lifflander <jliffla@sandia.gov>
2018-10-31 23:13:30 -0700

    #3: build: remove kokkos_DIR from build script (vt-auto-build)

--- build-checkpoint.sh
+++ build-checkpoint.sh
@@ -44,5 +44,7 @@ cmake ../checkpoint                                                    \
       -DCHECKPOINT_BUILD_EXAMPLES:bool=${build_tests}                  \
       -Dgtest_DIR=${gtest_directory}                                   \
       -DCMAKE_BUILD_TYPE=${build_mode}                                 \
-      -Dkokkos_DIR=/Users/jliffla/codes/kokkos-install-cmake/lib/      \
       -DCMAKE_EXPORT_COMPILE_COMMANDS=true
+
+
+#      -Dkokkos_DIR=/Users/jliffla/codes/kokkos-install-cmake/lib/      \
