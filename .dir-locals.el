eb04f19768f7fc7c02fbe8e8d0f369832a1cc919
Jonathan Lifflander <jliffla@sandia.gov>
2018-11-13 19:56:26 -0600

    #9: build project: commit emacs config file mod (should be untracked!)

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
 ((nil
-  (cmake-ide-project-dir . "/Users/jliffla/codes/serdes/checkpoint")
-  (cmake-ide-build-dir . "/Users/jliffla/codes/serdes/checkpoint-build")
+  (cmake-ide-project-dir . "/Users/jliffla/codes/vt/checkpoint")
+  (cmake-ide-build-dir . "/Users/jliffla/codes/vt/checkpoint-build")
   (cmake-ide-cmake-opts . " -DCMAKE_BUILD_TYPE=debug -DCMAKE_C_COMPILER=clang-mp-3.9 -DCMAKE_CXX_COMPILER=clang++-mp-3.9")
 ))
