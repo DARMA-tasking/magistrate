5fbd9f91a34b5775dccb32b1d78daebb1cf126aa
Jonathan Lifflander <jliffla@sandia.gov>
2018-11-13 23:11:09 -0600

    #11: atomic view: allow view trait Kokkos::MemoryTraits<Kokkos::Atomic>

--- src/container/view_traverse_manual.h
+++ src/container/view_traverse_manual.h
@@ -4,6 +4,7 @@
 
 #include "serdes_common.h"
 #include "serializers/serializers_headers.h"
+#include "container/view_traits_extract.h"
 
 #if KOKKOS_ENABLED_SERDES
 
@@ -30,21 +31,95 @@ namespace serdes {
  * correctly accesses the data regardless of the layout.
  */
 
+template <typename ViewT, typename = void>
+struct SerializeMemory;
+
+template <typename ViewT>
+using KokkosAtomicArchetype = typename std::is_same<
+  typename ViewT::traits::memory_traits, ::Kokkos::MemoryTraits<Kokkos::Atomic>
+>;
+
+template <typename ViewT>
+struct SerializeMemory<
+  ViewT,
+  std::enable_if_t<KokkosAtomicArchetype<ViewT>::value>
+>
+{
+  template <typename SerializerT>
+  static void apply(SerializerT& s, ViewT const& v) {
+    using DataType = typename ViewGetType<ViewT>::DataType;
+    if (s.isUnpacking()) {
+      DataType val;
+      s | val;
+      v.operator()().operator=(val);
+    } else {
+      auto const& val = v.operator()().operator const DataType();
+      //DataType const& val = v.operator()();
+      s | val;
+    }
+  }
+};
+
+template <typename ViewT>
+struct SerializeMemory<
+  ViewT,
+  std::enable_if_t<!KokkosAtomicArchetype<ViewT>::value>
+>
+{
+  template <typename SerializerT>
+  static void apply(SerializerT& s, ViewT const& v) {
+    s | v.operator()();
+  }
+};
+
 template <typename SerializerT, typename ViewType, std::size_t dims>
 struct TraverseManual;
 
 template <typename SerializerT, typename ViewType>
 struct TraverseManual<SerializerT,ViewType,0> {
   static void apply(SerializerT& s, ViewType const& v) {
-    s | v.operator()();
+    using BaseType = typename CountDims<ViewType>::BaseT;
+    using MemoryTraitsType = typename ViewType::traits::memory_traits;
+
+    SerializeMemory<ViewType>::apply(s,v);
+
+    //
+    // This code works for both atomic and non-atomic views but I've specially
+    // above for more clarity.
+    //
+    // if (s.isUnpacking()) {
+    //   BaseType val;
+    //   s | val;
+    //   v.operator()() = val;
+    // } else {
+    //   /*
+    //    * Be very careful here, do not use "auto const& val". If the view is
+    //    * atomic we need to get the conversion operator to `BaseType`. In full
+    //    * glory, we are invoking the following implicitly:
+    //    *
+    //    *   auto const& val = v.operator()().operator const BaseType();
+    //    */
+    //   BaseType const& val = v.operator()();
+    //   s | val;
+    // }
+    //
   }
 };
 
 template <typename SerializerT, typename ViewType>
 struct TraverseManual<SerializerT,ViewType,1> {
   static void apply(SerializerT& s, ViewType const& v) {
-    for (typename ViewType::size_type i = 0; i < v.extent(0); i++) {
-      s | v.operator()(i);
+    using BaseType = typename CountDims<ViewType>::BaseT;
+    using SizeType = typename ViewType::size_type;
+    for (SizeType i = 0; i < v.extent(0); i++) {
+      if (s.isUnpacking()) {
+        BaseType val;
+        s | val;
+        v.operator()(i) = val;
+      } else {
+        BaseType const& val = v.operator()(i);
+        s | val;
+      }
     }
   }
 };
@@ -52,9 +127,18 @@ struct TraverseManual<SerializerT,ViewType,1> {
 template <typename SerializerT, typename ViewType>
 struct TraverseManual<SerializerT,ViewType,2> {
   static void apply(SerializerT& s, ViewType const& v) {
-    for (typename ViewType::size_type i = 0; i < v.extent(0); i++) {
-      for (typename ViewType::size_type j = 0; j < v.extent(1); j++) {
-        s | v.operator()(i,j);
+    using BaseType = typename CountDims<ViewType>::BaseT;
+    using SizeType = typename ViewType::size_type;
+    for (SizeType i = 0; i < v.extent(0); i++) {
+      for (SizeType j = 0; j < v.extent(1); j++) {
+        if (s.isUnpacking()) {
+          BaseType val;
+          s | val;
+          v.operator()(i,j) = val;
+        } else {
+          BaseType const& val = v.operator()(i,j);
+          s | val;
+        }
       }
     }
   }
@@ -63,10 +147,19 @@ struct TraverseManual<SerializerT,ViewType,2> {
 template <typename SerializerT, typename ViewType>
 struct TraverseManual<SerializerT,ViewType,3> {
   static void apply(SerializerT& s, ViewType const& v) {
-    for (typename ViewType::size_type i = 0; i < v.extent(0); i++) {
-      for (typename ViewType::size_type j = 0; j < v.extent(1); j++) {
-        for (typename ViewType::size_type k = 0; k < v.extent(2); k++) {
-          s | v.operator()(i,j,k);
+    using BaseType = typename CountDims<ViewType>::BaseT;
+    using SizeType = typename ViewType::size_type;
+    for (SizeType i = 0; i < v.extent(0); i++) {
+      for (SizeType j = 0; j < v.extent(1); j++) {
+        for (SizeType k = 0; k < v.extent(2); k++) {
+          if (s.isUnpacking()) {
+            BaseType val;
+            s | val;
+            v.operator()(i,j,k) = val;
+          } else {
+            BaseType const& val = v.operator()(i,j,k);
+            s | val;
+          }
         }
       }
     }
@@ -76,11 +169,20 @@ struct TraverseManual<SerializerT,ViewType,3> {
 template <typename SerializerT, typename ViewType>
 struct TraverseManual<SerializerT,ViewType,4> {
   static void apply(SerializerT& s, ViewType const& v) {
-    for (typename ViewType::size_type i = 0; i < v.extent(0); i++) {
-      for (typename ViewType::size_type j = 0; j < v.extent(1); j++) {
-        for (typename ViewType::size_type k = 0; k < v.extent(2); k++) {
-          for (typename ViewType::size_type l = 0; l < v.extent(3); l++) {
-            s | v.operator()(i,j,k,l);
+    using BaseType = typename CountDims<ViewType>::BaseT;
+    using SizeType = typename ViewType::size_type;
+    for (SizeType i = 0; i < v.extent(0); i++) {
+      for (SizeType j = 0; j < v.extent(1); j++) {
+        for (SizeType k = 0; k < v.extent(2); k++) {
+          for (SizeType l = 0; l < v.extent(3); l++) {
+            if (s.isUnpacking()) {
+              BaseType val;
+              s | val;
+              v.operator()(i,j,k,l) = val;
+            } else {
+              BaseType const& val = v.operator()(i,j,k,l);
+              s | val;
+            }
           }
         }
       }
