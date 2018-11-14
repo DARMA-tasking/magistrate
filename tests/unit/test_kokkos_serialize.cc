3c6134fb806b49200d717f3eba773bbc92732469
Jonathan Lifflander <jliffla@sandia.gov>
2018-11-13 23:11:43 -0600

    #11: tests: add Kokkos::MemoryTraits<Kokkos::Atomic> to integration test

--- tests/unit/test_kokkos_serialize.cc
+++ tests/unit/test_kokkos_serialize.cc
@@ -556,9 +556,11 @@ static constexpr std::size_t const d3_a = 2, d3_b = 7, d3_c = 4;
 static constexpr std::size_t const d4_a = 2;
 
 struct Data : BaseData {
+  using AtomicTrait      = ::Kokkos::MemoryTraits<Kokkos::Atomic>;
+  using Kokkos_ViewType0 = ::Kokkos::View<int,      AtomicTrait>;
   using Kokkos_ViewType1 = ::Kokkos::View<int*,     Kokkos::LayoutLeft>;
   using Kokkos_ViewType2 = ::Kokkos::View<double**, Kokkos::LayoutRight>;
-  using Kokkos_ViewType3 = ::Kokkos::View<float***>;
+  using Kokkos_ViewType3 = ::Kokkos::View<float***, AtomicTrait>;
   using Kokkos_ViewType4 = ::Kokkos::View<int*[2]>;
   using DimType          = typename Kokkos_ViewType1::size_type;
 
@@ -567,10 +569,13 @@ struct Data : BaseData {
   explicit Data(DataConsTagType)
     : val1(gold_val1), val2(gold_val2), vec({gold_val3,gold_val3+1,gold_val3+2})
   {
+    Kokkos_ViewType0 v0_tmp("v0_tmp");
     Kokkos_ViewType1 v1_tmp("v1_tmp",d1_a);
     Kokkos_ViewType2 v2_tmp("v2_tmp",d2_a,d2_b);
     Kokkos_ViewType3 v3_tmp("v3_tmp",d3_a,d3_b,d3_c);
     Kokkos_ViewType4 v4_tmp("v4_tmp",d4_a);
+
+    v0_tmp.operator()() = v0val();
 
     for (DimType i = 0; i < d1_a; i++) {
       v1_tmp.operator()(i) = v1val(i);
@@ -591,6 +596,7 @@ struct Data : BaseData {
       v4_tmp.operator()(i,0) = v4val(i,0);//d4_a*i*2 + 1;
       v4_tmp.operator()(i,1) = v4val(i,1);//d4_a*i*2 + 2;
     }
+    v0 = v0_tmp;
     v1 = v1_tmp;
     v2 = v2_tmp;
     v3 = v3_tmp;
@@ -598,6 +604,9 @@ struct Data : BaseData {
   }
 
   /* Generators for creating expected data values */
+  static int v0val() {
+    return d1_a * 29;
+  }
   static int v1val(DimType i) {
     return d1_a * i;
   }
@@ -619,11 +628,13 @@ struct Data : BaseData {
     EXPECT_EQ(in.vec[0],gold_val3+0);
     EXPECT_EQ(in.vec[1],gold_val3+1);
     EXPECT_EQ(in.vec[2],gold_val3+2);
+    EXPECT_EQ(in.v0.size(),1);
     EXPECT_EQ(in.v1.size(),d1_a);
     EXPECT_EQ(in.v2.size(),d2_a*d2_b);
     EXPECT_EQ(in.v3.size(),d3_a*d3_b*d3_c);
     EXPECT_EQ(in.v4.size(),d4_a*2);
 
+    EXPECT_EQ(in.v0.operator()(), v0val());
     for (DimType i = 0; i < d1_a; i++) {
       EXPECT_EQ(in.v1.operator()(i), v1val(i));
     }
@@ -651,6 +662,7 @@ struct Data : BaseData {
 public:
   std::vector<int> vec = {};
   int val1 = 1, val2 = 2;
+  Kokkos_ViewType0 v0;
   Kokkos_ViewType1 v1;
   Kokkos_ViewType2 v2;
   Kokkos_ViewType3 v3;
@@ -670,7 +682,7 @@ void serialize(SerializerT& s, Data& data) {
   s | base_cls;
   s | data.vec;
   s | data.val1 | data.val2;
-  s | data.v1 | data.v2 | data.v3 | data.v4;
+  s | data.v0 | data.v1 | data.v2 | data.v3 | data.v4;
 }
 
 } /* end namespace serdes */
@@ -696,8 +708,6 @@ TEST_F(KokkosIntegrateTest, test_integrate_1) {
 
   auto ret = serialize<DataType>(test_data);
   auto out = deserialize<DataType>(std::move(ret));
-
-  //std::cout << "size=" << ret->getSize() << std::endl;
 
   Data::checkIsGolden(*out);
   Data::checkIsGolden(test_data);
