aac7db4bea4965b5bd1f831bc64484a23fe72339
Jonathan Lifflander <jliffla@sandia.gov>
2018-11-13 19:54:46 -0600

    #9: view equality: push short-circuit logic down entire eq compare stack

--- src/container/view_equality.h
+++ src/container/view_equality.h
@@ -23,10 +23,54 @@
 #include <tuple>
 #include <type_traits>
 #include <functional>
+#include <iostream>
 
-namespace serdes {
+/*
+ * Macro used for convenience to apply eq operator, obtain return result, and
+ * then return false if the eq operator returns false. This enables ViewEquality
+ * to terminate when it find inequality deep in the stack.
+ */
+#define SERDES_APPLY_OP(eq, opA, opB, reveal)                           \
+  do {                                                                  \
+    if (!eq(opA,opB)) {                                                 \
+      std::cout << "Failure on equal op: "                              \
+                << "\n\t info=(" << #reveal << ", " << reveal << ")"    \
+                << "\n\t opA=("  << #opA    << ", " << opA    << ")"    \
+                << "\n\t !="                                            \
+                << "\n\t opB=("  << #opB    << ", " << opB    << ")\n"; \
+      return false;                                                     \
+    }                                                                   \
+  } while (0);
 
-namespace detail {
+/*
+ * Implement a super simple std::tuple<...> printer for the above overload
+ * when the index as a std::tuple<...> is passed to reveal. This allows the
+ * above function to print out the index that fails the quality test:
+ */
+
+namespace std {
+
+template <typename Ch, typename Tr, typename Tuple, std::size_t... Is>
+void printTuple(
+  std::basic_ostream<Ch,Tr>& os, Tuple const& t, std::index_sequence<Is...>
+) {
+  using UnFoldType = int[];
+  (void)UnFoldType{((os << (Is == 0 ? "" : ", ") << std::get<Is>(t)), 0)...};
+}
+
+template <typename Ch, typename Tr, typename... Args>
+std::basic_ostream<Ch, Tr>& operator<<(
+  std::basic_ostream<Ch, Tr>& os, std::tuple<Args...> const& t
+) {
+  constexpr auto size = std::tuple_size<std::tuple<Args...>>::value;
+  os << "(";
+  printTuple(os, t, std::make_index_sequence<size>{});
+  return os << ")";
+}
+
+} /* end namespace std */
+
+namespace serdes { namespace detail {
 
 /*
  * Impl class in detail for comparing static to dynamic extents on a single view
@@ -37,66 +81,74 @@ struct ViewEqualityStatic {
   // Compare the static dimension to the dynamic extent: it must always be
   // equal for correctness
   template <typename T, unsigned N, unsigned M, typename... Args>
-  void operator()(Kokkos::View<T*[N][M],Args...> const& v, Callable eq) {
+  bool operator()(Kokkos::View<T*[N][M],Args...> const& v, Callable eq) {
     using ExtentType = decltype(v.extent(1));
-    eq(v.extent(1), static_cast<ExtentType>(N));
-    eq(v.extent(2), static_cast<ExtentType>(M));
+    SERDES_APPLY_OP(eq, v.extent(1), static_cast<ExtentType>(N), N);
+    SERDES_APPLY_OP(eq, v.extent(2), static_cast<ExtentType>(M), M);
+    return true;
   }
 
   template <typename T, unsigned N, unsigned M, typename... Args>
-  void operator()(Kokkos::View<T[N][M],Args...> const& v, Callable eq) {
+  bool operator()(Kokkos::View<T[N][M],Args...> const& v, Callable eq) {
     using ExtentType = decltype(v.extent(1));
-    eq(v.extent(0), static_cast<ExtentType>(N));
-    eq(v.extent(1), static_cast<ExtentType>(M));
+    SERDES_APPLY_OP(eq, v.extent(0), static_cast<ExtentType>(N), N);
+    SERDES_APPLY_OP(eq, v.extent(1), static_cast<ExtentType>(M), M);
+    return true;
   }
 
   template <typename T, unsigned N, typename... Args>
-  void operator()(Kokkos::View<T**[N],Args...> const& v, Callable eq) {
+  bool operator()(Kokkos::View<T**[N],Args...> const& v, Callable eq) {
     using ExtentType = decltype(v.extent(1));
-    eq(v.extent(2), static_cast<ExtentType>(N));
+    SERDES_APPLY_OP(eq, v.extent(2), static_cast<ExtentType>(N), N);
+    return true;
   }
 
   template <typename T, unsigned N, typename... Args>
-  void operator()(Kokkos::View<T*[N],Args...> const& v, Callable eq) {
+  bool operator()(Kokkos::View<T*[N],Args...> const& v, Callable eq) {
     using ExtentType = decltype(v.extent(1));
-    eq(v.extent(1), static_cast<ExtentType>(N));
+    SERDES_APPLY_OP(eq, v.extent(1), static_cast<ExtentType>(N), N);
+    return true;
   }
 
   template <typename T, unsigned N, typename... Args>
-  void operator()(Kokkos::View<T[N],Args...> const& v, Callable eq) {
+  bool operator()(Kokkos::View<T[N],Args...> const& v, Callable eq) {
     using ExtentType = decltype(v.extent(1));
-    eq(v.extent(0), static_cast<ExtentType>(N));
+    SERDES_APPLY_OP(eq, v.extent(0), static_cast<ExtentType>(N), N);
+    return true;
   }
 
   template <typename AnyT, typename... Args>
-  void operator()(Kokkos::View<AnyT,Args...> const& v, Callable eq) {
-    // no static dimension match
+  bool operator()(Kokkos::View<AnyT,Args...> const& v, Callable eq) {
+    // No static dimension to check against dynamic dimension, return true
+    return true;
   }
 
   template <typename AnyT, typename... Args>
-  void operator()(
+  bool operator()(
     Kokkos::Experimental::DynamicView<AnyT,Args...> const& v, Callable eq
   ) {
-    // no static dimension match
+    // No static dimension to check against dynamic dimension, return true
+    return true;
   }
 
 };
 
 struct DefaultEQ {
   template <typename T>
-  void operator()(T& a, T& b) const {
-    assert(a == b && "Must be equal");
-  }
-  template <typename T>
-  void operator()(T&& a, T&& b) const {
-    assert(a == b && "Must be equal");
+  bool operator()(T&& a, T&& b) const {
+    return a == b;
   }
 };
 
-} /* end namespace detail */
+}} /* end namespace serdes::detail */
+
+namespace serdes {
 
 /*
- * Check equality of two views: meta-data, internal data, etc.
+ *==============================================================================
+ *
+ *        Check equality of two views: meta-data, internal data, etc.
+ *
  *
  *   ========== Example w/default equality operator: =================
  *
@@ -106,22 +158,23 @@ struct DefaultEQ {
  *
  *     struct EqFunctor {
  *       template <typename T>
- *       void operator()(T& a, T& b) const { EXPECT_EQ(a,b); }
- *       template <typename T>
- *       void operator()(T&& a, T&& b) const { EXPECT_EQ(a,b); }
+ *       bool operator()(T&& a, T&& b) const {
+ *         return a == b;
+ *       }
  *     };
  *
  *     serdes::ViewEquality<ViewT>::template compare<EqFunctor>(view1,view2);
  *
  *  Note: if you specify your own equality functor, it needs to handle l-values
- *  and r-values based on how ViewEquality uses it. You can also specify a
- *  lambda or even two lambdas, one typed with the underlying data type of the
- *  view.
+ *  and r-values based on how ViewEquality uses it (so use a universal reference
+ *  as above or one may write multiple overloads for each type of
+ *  reference). You can also specify a lambda or even two lambdas, one typed
+ *  with the underlying data type of the view.
  *
  *  Depending on what your equality functor does, the comparison code will not
- *  stop when (in)equality is determined: it just invokes the function and does
- *  not have a return value to indicate incorrectness.
+ *  stop when (in)equality is determined unless the functor returns false.
  *
+ *==============================================================================
  */
 
 template <typename ViewT, typename ViewU = ViewT>
@@ -130,9 +183,13 @@ struct ViewEquality {
   using CountDimType = serdes::CountDims<ViewT>;
   using BaseType     = typename CountDimType::BaseT;
 
-  // Static check on the layout of the two views in question
+  /*
+   * Static check on the layout of the two views in question
+   */
   using LayoutT = typename ViewT::array_layout;
   using LayoutU = typename ViewU::array_layout;
+  // Do the static check right here: should break in instantiation if ViewT and
+  // ViewU do not have the same layout
   static_assert(std::is_same<LayoutT, LayoutU>::value, "Must be same layout");
 
   /*
@@ -142,18 +199,24 @@ struct ViewEquality {
    * actual internal data inside the views by invoking the quality functor
    */
   template <typename DataEq = detail::DefaultEQ, typename MetaEq = DataEq>
-  static void compare(
+  static bool compare(
     ViewT const& v1, ViewU const& v2, DataEq eqd = {}, MetaEq eqm = {}
   ) {
     // Check static dimension wrt to dynamic extents for both views
-    compareStaticDim(v1, eqm);
-    compareStaticDim(v2, eqm);
+    auto const static_v1_ret   = compareStaticDim(v1, eqm);
+    if (!static_v1_ret)   { return false; }
+    auto const static_v2_ret   = compareStaticDim(v2, eqm);
+    if (!static_v2_ret)   { return false; }
     // Compare the extents of v1 and v2
-    compareExtents(v1, v2, eqm);
+    auto const extent_test_ret = compareExtents(v1, v2, eqm);
+    if (!extent_test_ret) { return false; }
     // Compare the meta-data for v1 and v2
-    compareMeta(v1, v2, eqm);
+    auto const meta_test_ret   = compareMeta(v1, v2, eqm);
+    if (!meta_test_ret)   { return false; }
     // Compare the actual data in v1 and v2
-    compareData(v1, v2, eqd);
+    auto const data_test_ret   = compareData(v1, v2, eqd);
+    if (!data_test_ret)   { return false; }
+    return true;
   }
 
   /*
@@ -161,9 +224,9 @@ struct ViewEquality {
    * extents to the dynamic extents to ensure equality
    */
   template <typename EqT = detail::DefaultEQ>
-  static void compareStaticDim(ViewT const& v1, EqT c = {}) {
+  static bool compareStaticDim(ViewT const& v1, EqT c = {}) {
     detail::ViewEqualityStatic<ViewT,EqT> static_eq;
-    static_eq.operator()(v1,c);
+    return static_eq.operator()(v1,c);
   }
 
   /*
@@ -171,24 +234,26 @@ struct ViewEquality {
    * extents
    */
   template <typename EqT = detail::DefaultEQ>
-  static void compareExtents(ViewT const& v1, ViewU const& v2, EqT eq = {}) {
+  static bool compareExtents(ViewT const& v1, ViewU const& v2, EqT eq = {}) {
     auto const ndims_v1 = CountDims<ViewT>::numDims(v1);
     auto const ndims_v2 = CountDims<ViewU>::numDims(v2);
-    eq(ndims_v1,ndims_v2);
+    SERDES_APPLY_OP(eq, ndims_v1, ndims_v2, "num dims equal");
     for (auto i = 0; i < ndims_v1; i++) {
-      eq(v1.extent(i), v2.extent(i));
+      SERDES_APPLY_OP(eq, v1.extent(i), v2.extent(i), i);
     }
+    return true;
   }
 
   /*
    * This compares the view meta-data: size, label, contig, span
    */
   template <typename EqT = detail::DefaultEQ>
-  static void compareMeta(ViewT const& v1, ViewU const& v2, EqT eq = {}) {
-    eq(v1.label(),              v2.label());
-    eq(v1.size(),               v2.size());
-    eq(v1.span_is_contiguous(), v2.span_is_contiguous());
-    eq(v1.span(),               v2.span());
+  static bool compareMeta(ViewT const& v1, ViewU const& v2, EqT eq = {}) {
+    SERDES_APPLY_OP(eq, v1.label(),              v2.label(), "label");
+    SERDES_APPLY_OP(eq, v1.size(),               v2.size(), "size");
+    SERDES_APPLY_OP(eq, v1.span_is_contiguous(), v2.span_is_contiguous(), "contig");
+    SERDES_APPLY_OP(eq, v1.span(),               v2.span(), "span");
+    return true;
   }
 
   /*
@@ -198,22 +263,37 @@ struct ViewEquality {
    * essentially zipping the elements
    */
   template <typename EqT = detail::DefaultEQ>
-  static void compareData(ViewT const& v1, ViewU const& v2, EqT eq) {
-    using DataType     = typename serdes::ViewGetType<ViewT>::DataType;
-    using CountDimType = serdes::CountDims<ViewT>;
-    using BaseType     = typename CountDimType::BaseT;
-    using TupleType    = std::tuple<ViewT,ViewT>;
+  static bool compareData(ViewT const& v1, ViewU const& v2, EqT eq) {
+    using serdes::TraverseRecursive;
+
+    using DataType      = typename serdes::ViewGetType<ViewT>::DataType;
+    using CountDimType  = serdes::CountDims<ViewT>;
+    using BaseType      = typename CountDimType::BaseT;
+    using TupleType     = std::tuple<ViewT,ViewT>;
 
     constexpr auto dims = CountDimType::dynamic;
 
-    // This is not strictly needed, but it might make this easier in the future
-    auto fn = [eq](BaseType& elm1, BaseType& elm2){
-      eq(elm1,elm2);
+    using RecurType     = TraverseRecursive<TupleType,DataType,dims>;
+    using IndexType     = typename RecurType::IndexType;
+
+    // Extract the index type above with the default (phony) op to extract the
+    // IndexType, which is needed below without using "auto" for lambda
+    // parameters, which I am trying to avoid.
+    auto fn = [eq](IndexType index, BaseType& elm1, BaseType& elm2){
+      auto const is_equal = eq(elm1,elm2);
+      if (!is_equal) {
+        SERDES_APPLY_OP(eq, elm1, elm2, index);
+      }
+      return is_equal;
     };
 
-    serdes::TraverseRecursive<TupleType,DataType,dims,decltype(fn)>::apply(
+    // Returns the number of comparisons in the inner data that returned false
+    using FnT = decltype(fn);
+    auto const neq = TraverseRecursive<TupleType,DataType,dims,FnT>::apply(
       std::make_tuple(v1,v2),fn
     );
+
+    return neq == 0;
   }
 
 };
--- src/container/view_traverse_ndim.h
+++ src/container/view_traverse_ndim.h
@@ -17,6 +17,7 @@
 #include <tuple>
 #include <cassert>
 #include <type_traits>
+#include <cstdlib>
 
 namespace serdes { namespace detail {
 
@@ -75,40 +76,55 @@ struct TraverseRecurImplBase {
   using ViewIsTuple    = typename std::enable_if<IsTuple<U>::value,ViewT>::type;
   template <typename U>
   using ViewNotTuple   = typename std::enable_if<!IsTuple<U>::value,ViewT>::type;
+  using CountType      = uint64_t;
 
-  using TupleChildT = decltype(
+  using TupleChildT    = decltype(
     std::tuple_cat(
       std::declval<TupleT>(),
       std::declval<std::tuple<SizeType>>()
     )
   );
-  using ChildT = TraverseRecurImpl<ViewT,T,TupleChildT,d-1,Callable>;
+
+  using ChildT         = TraverseRecurImpl<ViewT,T,TupleChildT,d-1,Callable>;
+  using IndexTupleType = typename ChildT::IndexTupleType;
 
   template <typename U = ViewT>
-  static void applyImpl(
+  static CountType applyImpl(
     ViewT const& view, unsigned nd, TupleT idx, Callable call,
     ViewIsTuple<U>* x_ = nullptr
   ) {
     auto const ex1 = std::get<0>(view).extent(nd-d);
     auto const ex2 = std::get<1>(view).extent(nd-d);
     assert(ex1 == ex2 && "Matching extents must be equal");
+    CountType neq = 0;
     for (SizeType i = 0; i < ex1; i++) {
-      ChildT::apply(view,nd,std::tuple_cat(idx,std::tuple<SizeType>(i)),call);
+      auto const ret = ChildT::apply(
+        view,nd,std::tuple_cat(idx,std::tuple<SizeType>(i)),call
+      );
+      neq += !ret;
     }
+    return neq;
   }
 
   template <typename U = ViewT>
-  static void applyImpl(
+  static CountType applyImpl(
     ViewT const& view, unsigned nd, TupleT idx, Callable call,
     ViewNotTuple<U>* x_ = nullptr
   ) {
+    CountType neq = 0;
     for (SizeType i = 0; i < view.extent(nd-d); i++) {
-      ChildT::apply(view,nd,std::tuple_cat(idx,std::tuple<SizeType>(i)),call);
+      auto const ret = ChildT::apply(
+        view,nd,std::tuple_cat(idx,std::tuple<SizeType>(i)),call
+      );
+      neq += !ret;
     }
+    return neq;
   }
 
-  static void apply(ViewT const& view, unsigned nd, TupleT idx, Callable call) {
-    applyImpl<ViewT>(view,nd,idx,call);
+  static CountType apply(
+    ViewT const& view, unsigned nd, TupleT idx, Callable call
+  ) {
+    return applyImpl<ViewT>(view,nd,idx,call);
   }
 };
 
@@ -142,6 +158,7 @@ struct TraverseRecurImpl {
   using ViewNotTuple = typename std::enable_if<!IsTuple<U>::value,ViewT>::type;
   template <typename U>
   using GetBaseType  = typename CountDims<U>::BaseT;
+  using IndexTupleType = TupleT;
 
   // Unwind the inner tuple for operator()(...)
   template <typename ViewU, std::size_t... I>
@@ -160,16 +177,16 @@ struct TraverseRecurImpl {
 
   // If it's a std::tuple<Kokkos::vie<T>...>, first unwind the outer tuple
   template <std::size_t... I>
-  static void dispatchViewTuple(
+  static bool dispatchViewTuple(
     ViewT const& view, Callable call, TupleT tup, std::index_sequence<I...>
   ) {
     // Dispatch the call operator on the view
-    call(expandTupleToOp(std::get<I>(view),tup)...);
+    return call(tup,expandTupleToOp(std::get<I>(view),tup)...);
   }
 
   // Test whether the ViewT is actually a std::tuple<Kokkos::View<T>...>
   template <typename U = ViewT>
-  static void dispatchViewType(
+  static bool dispatchViewType(
     ViewT const& view, Callable call, TupleT tup, ViewIsTuple<U>* x_ = nullptr
   ) {
     constexpr auto size = std::tuple_size<ViewT>::value;
@@ -177,26 +194,36 @@ struct TraverseRecurImpl {
   }
 
   template <typename U = ViewT>
-  static void dispatchViewType(
+  static bool dispatchViewType(
     ViewT const& view, Callable call, TupleT tup, ViewNotTuple<U>* x_ = nullptr
   ) {
     call(expandTupleToOp(view,tup));
+    return true;
   }
 
-  static void apply(ViewT const& view, unsigned, TupleT tup, Callable call) {
+  static bool apply(ViewT const& view, unsigned, TupleT tup, Callable call) {
     return dispatchViewType(view,call,tup);
   }
 };
 
+struct DefaultRecurOP {
+  template <typename T, typename U>
+  bool operator()(U, T, T) const { assert(0); }
+};
 
 }} /* end namespace serdes::detail */
 
 namespace serdes {
 
-template <typename ViewT, typename T, unsigned nd, typename Callable>
+template <
+  typename ViewT, typename T, unsigned nd,
+  typename Callable = detail::DefaultRecurOP
+>
 struct TraverseRecursive {
-  using RecurT = detail::TraverseRecurImpl<ViewT,T,std::tuple<>,nd,Callable>;
-  static void apply(ViewT const& view, Callable call) {
+  using RecurT    = detail::TraverseRecurImpl<ViewT,T,std::tuple<>,nd,Callable>;
+  using CountType = typename RecurT::CountType;
+  using IndexType = typename RecurT::IndexTupleType;
+  static CountType apply(ViewT const& view, Callable call) {
     return RecurT::apply(view,nd,std::tuple<>{},call);
   }
 };
