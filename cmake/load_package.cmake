bde64244538a024907b10ed6f4727e6f5814e53f
Jonathan Lifflander <jliffla@sandia.gov>
2018-11-20 18:19:41 -0800

    #6: build: rewrite find_package_local to have extra search paths
    The primary problem was that when a <pkg-name>_DIR is set, cmake uses
    it in find_package and actually *changes* the value of the variable to
    the directory actually found!
    
    Thus,
    
      find_package_local(kokkos "${kokkos_DIR}/cmake/kokkos" ...)
    
    works properly the first time, but then fails when cmake is
    re-run. This bug was very confusing because cmake changed the
    behaviour with <pkg-name>_DIR being set.
    
    Thus, find_package_local now takes alterative paths and automaticall
    tries multiple until one is found that works:
    
      find_package_local(
        kokkos "${kokkos_DIR}" Kokkos
        "cmake/Kokkos" "CMake/Kokkos" # New arguments
      )
    
    Additionally, the installed `checkpointConfig.cmake.in` file was
    incorrect. It references ${kokkos_DIR} to export the configuration,
    causing a failure to load.

---
+++ cmake/load_package.cmake
@@ -0,0 +1,96 @@
+
+macro(require_pkg_directory pkg_name pkg_user_name)
+  #message(STATUS "require_directory: name=${pkg_name}")
+  option(${pkg_name}_DIR "Root folder for ${pkg_user_name} installation" OFF)
+  if (NOT ${pkg_name}_DIR)
+    message(
+      FATAL_ERROR "Please specify ${pkg_user_name} library installation root"
+      " with -D${pkg_name}_DIR="
+    )
+  endif()
+endmacro(require_pkg_directory)
+
+macro(find_package_local pkg_name pkg_directory pkg_other_name)
+  message(
+    STATUS "find_package_local: pkg name=\"${pkg_name}\", "
+           "directory=\"${pkg_directory}\""
+  )
+
+  # Rest of the arguments are potential relative search paths wrt the
+  # ${pkg_directory}
+  set(prefix_args ${ARGN})
+
+  # Default search paths: root, /cmake and /CMake subdirectories
+  list(APPEND prefix_args "/" "/cmake" "/CMake")
+
+  # Whether we loaded the package in the following loop with find_package()
+  set(${pkg_name}_PACKAGE_LOADED 0)
+
+  foreach(prefix ${prefix_args})
+    set(potential_path ${pkg_directory}/${prefix})
+    set(${pkg_name}_DIR ${potential_path})
+    # message("prefix: ${potential_path}")
+    if (EXISTS "${${pkg_name}_DIR}${pkg_prefix}")
+      # message(STATUS "find_package_local: trying path: ${potential_path}")
+
+      # Search locally only for package based on the user's supplied path; if
+      # this fails try to next one. Even if the directory exists (tested above)
+      # this might fail if a directory does not have the config file
+      find_package(
+        ${pkg_name}
+        PATHS ${potential_path}
+        NAMES ${pkg_name} ${pkg_other_name}
+        NO_CMAKE_PACKAGE_REGISTRY
+        NO_CMAKE_BUILDS_PATH
+        NO_CMAKE_SYSTEM_PATH
+        NO_CMAKE_SYSTEM_PACKAGE_REGISTRY
+        QUIET
+      )
+
+      # Break out of the search loop now that we have found the path
+      if (${${pkg_name}_FOUND})
+        message(STATUS "find_package_local: found with prefix: ${prefix}")
+        set(${pkg_name}_PACKAGE_LOADED 1)
+        break()
+      endif()
+    endif()
+  endforeach()
+
+  if (NOT ${${pkg_name}_PACKAGE_LOADED})
+    message(STATUS "find_package_local: can not find package: ${pkg_name}")
+
+    foreach(prefix ${prefix_args})
+      set(path ${${pkg_name}_DIR}/${prefix})
+      message(STATUS "find_package_local: searched: ${path}")
+    endforeach()
+
+    message(
+      FATAL_ERROR "find_package_local: can not find package: ${pkg_name}"
+      " tried to find_package(..) with above search paths"
+    )
+  endif()
+endmacro(find_package_local)
+
+macro(optional_pkg_directory pkg_name pkg_user_name)
+  #message(STATUS "optional_pkg_directory: name=${pkg_name}")
+  option(${pkg_name}_DIR "Root folder for ${pkg_user_name} installation" OFF)
+  if (NOT ${pkg_name}_DIR)
+    message(
+      STATUS
+      "Path for ${pkg_user_name} library (optional) not specified "
+      "with -D${pkg_name}_DIR="
+    )
+    message(
+      STATUS
+      "Building without ${pkg_user_name} library"
+    )
+    set(${pkg_name}_DIR_FOUND 0)
+  else()
+    message(
+      STATUS
+      "Path for ${pkg_user_name} library (optional) specified "
+      "with -D${pkg_name}_DIR=${${pkg_name}_DIR}"
+    )
+    set(${pkg_name}_DIR_FOUND 1)
+  endif()
+endmacro(optional_pkg_directory)
