171b878633a67b2c3747f2fc753a2a7ff0f5c056
Perrinel Mériadeg <meriadeg.perrinel@ng-analytics.com>
2018-12-01 11:51:09 +0100

    #16: kokkos test: Add const tests for 2D and 2DMPI
    The test check if the tuple is const or not and do the necessary to initialize and test the serialization

--- tests/unit/test_kokkos_serialize_2d.cc
+++ tests/unit/test_kokkos_serialize_2d.cc
@@ -12,39 +12,59 @@ TYPED_TEST_CASE_P(KokkosViewTest2D);
 TYPED_TEST_P(KokkosViewTest2D, test_2d_any) {
   using namespace serialization::interface;
 
-  using LayoutType = typename std::tuple_element<0,TypeParam>::type;
-  using DataType   = typename std::tuple_element<1,TypeParam>::type;
-  using ViewType   = Kokkos::View<DataType, LayoutType>;
-
+  using LayoutType        = typename std::tuple_element<0,TypeParam>::type;
+  using DataType          = typename std::tuple_element<1,TypeParam>::type;
+  using ViewType          = Kokkos::View<DataType, LayoutType>;
+  using NonConstT         = typename ViewType::traits::non_const_data_type;
+  using NonConstViewType  = Kokkos::View<NonConstT, LayoutType>;
+  using ConstT         = typename ViewType::traits::const_data_type;
+  using ConstViewType  = Kokkos::View<ConstT, LayoutType>;
   static constexpr size_t const N = 23;
   static constexpr size_t const M = 32;
 
-  constexpr auto dyn = serdes::CountDims<ViewType,DataType>::dynamic;
-
   LayoutType layout = layout2d<LayoutType>(N,M);
-  //dyn == 2 ? layout2d<LayoutType>(N,M) : layout1d<LayoutType>(N);
-  ViewType in_view("test-2D-some-string", layout);
 
+  NonConstViewType in_view("test-2D-some-string", layout);
   init2d(in_view);
 
-  auto ret = serialize<ViewType>(in_view);
-  auto out_view = deserialize<ViewType>(ret->getBuffer(), ret->getSize());
-  auto const& out_view_ref = *out_view;
+  if(std::is_same<NonConstViewType, ViewType>::value)
+  {
+    auto ret = serialize<NonConstViewType>(in_view);
+    auto out_view = deserialize<NonConstViewType>(ret->getBuffer(), ret->getSize());
+    auto const& out_view_ref = *out_view;
+    #if SERDES_USE_ND_COMPARE
+      compareND(in_view, out_view_ref);
+    #else
+      compare2d(in_view, out_view_ref);
+    #endif
+  }
+  else
+  {
+    ConstViewType const_in_view = in_view;
+    auto ret = serialize<ConstViewType>(const_in_view);
+    auto out_view = deserialize<ConstViewType>(ret->getBuffer(), ret->getSize());
+    auto const& out_view_ref = *out_view;
 
-#if SERDES_USE_ND_COMPARE
-  compareND(in_view, out_view_ref);
-#else
-  compare2d(in_view, out_view_ref);
-#endif
+      // Uncomment to make the test failed
+    #if SERDES_USE_ND_COMPARE
+     compareND(const_in_view, out_view_ref);
+    #else
+     compare2d(const_in_view, out_view_ref);
+    #endif
+  }
+
 }
 
 REGISTER_TYPED_TEST_CASE_P(KokkosViewTest2D, test_2d_any);
 
 #if DO_UNIT_TESTS_FOR_VIEW
 
-INSTANTIATE_TYPED_TEST_CASE_P(test_2d_L, KokkosViewTest2D, Test2DTypesLeft);
-INSTANTIATE_TYPED_TEST_CASE_P(test_2d_R, KokkosViewTest2D, Test2DTypesRight);
-INSTANTIATE_TYPED_TEST_CASE_P(test_2d_S, KokkosViewTest2D, Test2DTypesStride);
+INSTANTIATE_TYPED_TEST_CASE_P(test_2d_L,   KokkosViewTest2D, Test2DTypesLeft);
+INSTANTIATE_TYPED_TEST_CASE_P(test_2d_R,   KokkosViewTest2D, Test2DTypesRight);
+INSTANTIATE_TYPED_TEST_CASE_P(test_2d_S,   KokkosViewTest2D, Test2DTypesStride);
+INSTANTIATE_TYPED_TEST_CASE_P(test_2d_L_C, KokkosViewTest2D, Test2DConstTypesLeft);
+INSTANTIATE_TYPED_TEST_CASE_P(test_2d_R_C, KokkosViewTest2D, Test2DConstTypesRight);
+INSTANTIATE_TYPED_TEST_CASE_P(test_2d_S_C, KokkosViewTest2D, Test2DConstTypesStride);
 
 #endif
 #endif
--- tests/unit/tests_mpi/test_kokkos_serialize_2d_mpi.cc
+++ tests/unit/tests_mpi/test_kokkos_serialize_2d_mpi.cc
@@ -10,31 +10,44 @@ TYPED_TEST_CASE_P(KokkosViewTest2DMPI);
 TYPED_TEST_P(KokkosViewTest2DMPI, test_2d_any) {
   using namespace serialization::interface;
 
-  using LayoutType = typename std::tuple_element<0,TypeParam>::type;
-  using DataType   = typename std::tuple_element<1,TypeParam>::type;
-  using ViewType   = Kokkos::View<DataType, LayoutType>;
-
+  using LayoutType        = typename std::tuple_element<0,TypeParam>::type;
+  using DataType          = typename std::tuple_element<1,TypeParam>::type;
+  using ViewType          = Kokkos::View<DataType, LayoutType>;
+  using NonConstT         = typename ViewType::traits::non_const_data_type;
+  using NonConstViewType  = Kokkos::View<NonConstT, LayoutType>;
+  using ConstT         = typename ViewType::traits::const_data_type;
+  using ConstViewType  = Kokkos::View<ConstT, LayoutType>;
   static constexpr size_t const N = 23;
   static constexpr size_t const M = 32;
 
   constexpr auto dyn = serdes::CountDims<ViewType,DataType>::dynamic;
 
   LayoutType layout = layout2d<LayoutType>(N,M);
-  //dyn == 2 ? layout2d<LayoutType>(N,M) : layout1d<LayoutType>(N);
-  ViewType in_view("test-2D-some-string", layout);
+  NonConstViewType in_view("test-2D-some-string", layout);
 
   init2d(in_view);
 
-  serialiseDeserializeBasic<ViewType>(in_view, &compare2d<ViewType>);
+  if(std::is_same<NonConstViewType, ViewType>::value)
+  {
+    serialiseDeserializeBasic<NonConstViewType>(in_view, &compare2d<NonConstViewType>);
+  }
+  else
+  {
+    ConstViewType const_in_view = in_view;
+    serialiseDeserializeBasic<ConstViewType>(const_in_view, &compare2d<ConstViewType>);
+  }
 }
 
 REGISTER_TYPED_TEST_CASE_P(KokkosViewTest2DMPI, test_2d_any);
 
 #if DO_UNIT_TESTS_FOR_VIEW
 
-INSTANTIATE_TYPED_TEST_CASE_P(test_2d_L, KokkosViewTest2DMPI, Test2DTypesLeft);
-INSTANTIATE_TYPED_TEST_CASE_P(test_2d_R, KokkosViewTest2DMPI, Test2DTypesRight);
-INSTANTIATE_TYPED_TEST_CASE_P(test_2d_S, KokkosViewTest2DMPI, Test2DTypesStride);
+INSTANTIATE_TYPED_TEST_CASE_P(test_2d_L,   KokkosViewTest2DMPI, Test2DTypesLeft);
+INSTANTIATE_TYPED_TEST_CASE_P(test_2d_R,   KokkosViewTest2DMPI, Test2DTypesRight);
+INSTANTIATE_TYPED_TEST_CASE_P(test_2d_S,   KokkosViewTest2DMPI, Test2DTypesStride);
+INSTANTIATE_TYPED_TEST_CASE_P(test_2d_L_C, KokkosViewTest2DMPI, Test2DConstTypesLeft);
+INSTANTIATE_TYPED_TEST_CASE_P(test_2d_R_C, KokkosViewTest2DMPI, Test2DConstTypesRight);
+INSTANTIATE_TYPED_TEST_CASE_P(test_2d_S_C, KokkosViewTest2DMPI, Test2DConstTypesStride);
 
 #endif
 #endif
