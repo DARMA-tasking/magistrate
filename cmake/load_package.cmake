b78260dd94759082945f9dfb1492a8179aba5e34
Nicole Lemaster Slattengren <nlslatt@sandia.gov>
2019-02-21 13:23:47 -0800

    #23: let optional_pkg_directory assume availability when has parent build system

--- cmake/load_package.cmake
+++ cmake/load_package.cmake
@@ -93,26 +93,40 @@ macro(find_package_local pkg_name pkg_directory pkg_other_name)
   endif()
 endmacro(find_package_local)
 
-macro(optional_pkg_directory pkg_name pkg_user_name)
-  #message(STATUS "optional_pkg_directory: name=${pkg_name}")
-  option(${pkg_name}_DIR "Root folder for ${pkg_user_name} installation" OFF)
-  if (NOT ${pkg_name}_DIR)
-    message(
-      STATUS
-      "Path for ${pkg_user_name} library (optional) not specified "
-      "with -D${pkg_name}_DIR="
-    )
-    message(
-      STATUS
-      "Building without ${pkg_user_name} library"
-    )
-    set(${pkg_name}_DIR_FOUND 0)
+macro(optional_pkg_directory pkg_name pkg_user_name assume_found_if_hasparent)
+  get_directory_property(hasParent PARENT_DIRECTORY)
+  if(hasParent)
+    # Skip MOST of this logic when this macro was not invoked from the
+    # top-level CMakeLists.txt file under the assumption that this
+    # package was dropped into another build system using add_subdirectory().
+    # Note that this will also skip if you call this macro from
+    # a subdirectory in your own package, so just don't do it!
+
+    if(${assume_found_if_hasparent})
+      # Assume that the package is available even if the directory wasn't specified
+      set(${pkg_name}_DIR_FOUND 1)
+    endif()
   else()
-    message(
-      STATUS
-      "Path for ${pkg_user_name} library (optional) specified "
-      "with -D${pkg_name}_DIR=${${pkg_name}_DIR}"
-    )
-    set(${pkg_name}_DIR_FOUND 1)
+    #message(STATUS "optional_pkg_directory: name=${pkg_name}")
+    option(${pkg_name}_DIR "Root folder for ${pkg_user_name} installation" OFF)
+    if (NOT ${pkg_name}_DIR)
+      message(
+        STATUS
+        "Path for ${pkg_user_name} library (optional) not specified "
+        "with -D${pkg_name}_DIR="
+      )
+      message(
+        STATUS
+        "Building without ${pkg_user_name} library"
+      )
+      set(${pkg_name}_DIR_FOUND 0)
+    else()
+      message(
+        STATUS
+        "Path for ${pkg_user_name} library (optional) specified "
+        "with -D${pkg_name}_DIR=${${pkg_name}_DIR}"
+      )
+      set(${pkg_name}_DIR_FOUND 1)
+    endif()
   endif()
 endmacro(optional_pkg_directory)
