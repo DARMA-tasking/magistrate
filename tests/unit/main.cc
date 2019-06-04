f3d642f5c731d240eda1d793cdbd0f3d13747a4c
Jonathan Lifflander <jliffla@sandia.gov>
2019-06-04 12:44:00 -0700

    #60: cuda: fix multiple init/finalize not allowed with cuda

--- tests/unit/main.cc
+++ tests/unit/main.cc
@@ -8,14 +8,29 @@
 #endif
 #include "test_harness.h"
 
+#if KOKKOS_ENABLED_SERDES
+#include <Kokkos_Core.hpp>
+#endif
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
 #if HAVE_MPI
   ::testing::AddGlobalTestEnvironment(new MPIEnvironment(&argc, argv));
 #endif
 
-  serdes::tests::unit::TestHarness::store_cmdline_args(argc, argv);
-  int const ret = RUN_ALL_TESTS();
+  int ret = 0;
+  {
+#if KOKKOS_ENABLED_SERDES
+    Kokkos::initialize();
+#endif
+
+    serdes::tests::unit::TestHarness::store_cmdline_args(argc, argv);
+    ret = RUN_ALL_TESTS();
+
+#if KOKKOS_ENABLED_SERDES
+    Kokkos::finalize();
+#endif
+  }
 
   return ret;
 }
