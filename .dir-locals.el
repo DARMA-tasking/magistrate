1d7459ed666d5150b3289c35ef255eeb880121ce
Jonathan Lifflander <jliffla@sandia.gov>
2020-09-15 21:47:53 -0700

    git: remove dir locals which never should be been added

--- .dir-locals.el
+++
@@ -1,5 +0,0 @@
-((nil
-  (cmake-ide-project-dir . "/Users/jliffla/codes/vt/checkpoint")
-  (cmake-ide-build-dir . "/Users/jliffla/codes/vt/checkpoint-build")
-  (cmake-ide-cmake-opts . " -DCMAKE_BUILD_TYPE=debug -DCMAKE_C_COMPILER=clang-mp-3.9 -DCMAKE_CXX_COMPILER=clang++-mp-3.9")
-))
