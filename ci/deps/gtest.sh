ca67f404b041e4c098ffaa0a0a54e120030aee84
Jonathan Lifflander <jliffla@sandia.gov>
2020-11-19 16:04:13 -0800

    #129: CI: write script for pulling dependencies automatically

---
+++ ci/deps/gtest.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -exo pipefail
+
+if test $# -lt 1
+then
+    echo "usage: ./$0 <gtest-version> <build-dir>"
+    exit 1
+fi
+
+gtest_version=$1
+gtest_zip_name=release-${gtest_version}.zip
+
+build_dir=$2
+
+echo "${gtest_version}"
+echo "${gtest_zip_name}"
+
+wget http://github.com/google/googletest/archive/${gtest_zip_name}
+
+unzip ${gtest_zip_name}
+
+mkdir -p "${build_dir}"
+pushd "${build_dir}"
+
+export gtest=/googletest-release-${gtest_version}
+export gtest_build=${build_dir}/gtest
+mkdir -p "$gtest_build"
+cd "$gtest_build"
+mkdir build
+cd build
+cmake -G "${CMAKE_GENERATOR:-Ninja}" \
+      -DCMAKE_INSTALL_PREFIX="$gtest_build/install" \
+      "$gtest"
+cmake --build . --target install
