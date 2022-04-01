4e4a5261b4c5235554b8b07216ba265b0ad1f99a
Cezary Skrzyński <cezary.skrzynski@ng-analytics.com>
2022-04-01 18:23:30 +0200

    #233: CI: generate list of modified files correctly

--- scripts/check_containers.sh
+++ scripts/check_containers.sh
@@ -2,11 +2,11 @@
 # Check if there were any dockerfiles modified in the latest pull request.
 
 diff_latest() {
-  git diff --name-only "$(git log -1 --merges --pretty=format:%H)"..HEAD
+  git diff --name-only origin/develop...HEAD
 }
 
 # Copy the diff output to stderr to list the modified files
-if diff_latest | tee /dev/stderr | grep -i dockerfile > /dev/null
+if diff_latest | tee >(cat >&2) | grep -i dockerfile > /dev/null
 then
     echo "docker-compose build --pull"
 else
