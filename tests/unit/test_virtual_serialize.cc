bba1f266c358feb92159c0017e91c779c71fa4cc
Arkadiusz Szczepkowicz <arek.szczepkowicz@ng-analytics.com>
2024-07-23 14:03:02 +0200

    #268: Add tests for deserialization of the polymorphic types

--- tests/unit/test_virtual_serialize.cc
+++ tests/unit/test_virtual_serialize.cc
@@ -611,6 +611,28 @@ INSTANTIATE_TYPED_TEST_CASE_P(
   test_virtual_serialize_inst, TestVirtualSerialize, ConstructTypes,
 );
 
+/*
+ * Test for deserialization when using the base class type
+ */
+
+using TestDeserializationFromBase = TestHarness;
+
+template<typename Base, typename Derived>
+void testDeserializationFromBase() {
+  std::unique_ptr<Base> task(new Derived(TEST_CONSTRUCT{}));
+  auto ret = checkpoint::serialize<Base>(*task);
+  auto out = checkpoint::deserialize<Base>(std::move(ret));
+
+  EXPECT_TRUE(nullptr != out);
+  out->check();
+}
+
+TEST_F(TestDeserializationFromBase, test_deserilization_from_base) {
+  testDeserializationFromBase<test_1::TestBase, test_1::TestDerived2>();
+  testDeserializationFromBase<test_2::TestBase, test_2::TestDerived2>();
+  testDeserializationFromBase<test_3::TestBase, test_3::TestDerived2>();
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
