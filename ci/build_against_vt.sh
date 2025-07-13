20461fe58217638edf9da233ff9728e147b7b5ac
Jacob Domagala <domagala.jacob@gmail.com>
2025-07-13 12:36:17 +0200

    #389: Try using only build artifacts for CI builds' output

--- ci/build_against_vt.sh
+++ ci/build_against_vt.sh
@@ -105,3 +105,8 @@ then
     { echo -e "===\n=== ccache statistics after build\n==="; } 2>/dev/null
     ccache -s
 fi
+
+if [ -n "${CI_CCACHE_DIR:-}" ]; then
+    mkdir -p "${CI_CCACHE_DIR}"
+    cp -a "${CCACHE_DIR}/." "${CI_CCACHE_DIR}/"
+fi
