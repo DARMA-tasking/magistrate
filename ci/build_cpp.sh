163f924c4aa747b7d894985a25fa235869ad6564
Jacob Domagala <domagala.jacob@gmail.com>
2025-08-12 11:35:08 +0200

    #398: Modify build_cpp.sh to generate cmake/build logs

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -46,6 +46,7 @@ cmake -G "${CMAKE_GENERATOR:-Ninja}" \
       -DKokkos_ROOT="${KOKKOS_ROOT}" \
       -DKokkosKernels_ROOT="${KOKKOS_KERNELS_ROOT}" \
       "$CHECKPOINT"
+cmake_conf_ret=$?
 
 if test "${MAGISTRATE_DOXYGEN_ENABLED:-0}" -eq 1
 then
@@ -71,9 +72,52 @@ then
         git commit --allow-empty -m "Update magistrate_docs (auto-build)"
         git push origin master
     fi
+elif test "${VT_CI_BUILD:-0}" -eq 1
+then
+    # Generate output file with compilation warnings and errors
+    GENERATOR=$(cmake -L . | grep USED_CMAKE_GENERATOR:STRING | cut -d"=" -f2)
+    OUTPUT="$VT_BUILD"/compilation_errors_warnings.out
+    OUTPUT_TMP="$OUTPUT".tmp
+    WARNS_ERRS=""
+
+    # Unfortunately Ninja doesn't output compilation warnings and errors to stderr
+    # so it needs special treatment
+    if test "$GENERATOR" = "Ninja"
+    then
+        # To easily tell if compilation of given file succeeded special progress bar is used
+        # (controlled by variable NINJA_STATUS)
+        export NINJA_STATUS="[ninja][%f/%t] "
+        time cmake --build . --target "${target}" | tee "$OUTPUT_TMP"
+        compilation_ret=${PIPESTATUS[0]}
+        sed -i '/ninja: build stopped:/d' "$OUTPUT_TMP"
+
+        # Now every line that doesn't start with [ninja][number]/[number] is an error or a warning
+        WARNS_ERRS=$(grep -Ev '^(\[ninja\]\[[[:digit:]]+\/[[:digit:]]+\])|(--) .*$' "$OUTPUT_TMP" || true)
+    elif test "$GENERATOR" = "Unix Makefiles"
+    then
+        # Gcc outputs warnings and errors to stderr, so there's not much to do
+        time cmake --build . --target "${target}" 2> >(tee "$OUTPUT_TMP")
+        compilation_ret=$?
+        WARNS_ERRS=$(cat "$OUTPUT_TMP")
+    fi
+
+    echo "$WARNS_ERRS" > "$OUTPUT"
 else
     time cmake --build . --target "${target}"
+    compilation_ret=$?
 fi
 
 echo -e "===\n=== ccache statistics after build\n==="
 ccache -s
+
+
+# Exit with error code if there was any
+if test "$cmake_conf_ret" -ne 0
+then
+    echo "There was an error during CMake configuration"
+    exit "$cmake_conf_ret"
+elif test "$compilation_ret" -ne 0
+then
+    echo "There was an error during compilation"
+    exit "$compilation_ret"
+fi
